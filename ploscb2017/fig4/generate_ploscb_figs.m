10dc1cc22423a1b6be279cb11b96978cc1ec21a6
Alex Dewar <a.dewar@sussex.ac.uk>
2017-12-03 20:20:25 +0000

    add fig 4 code

---
+++ ploscb2017/fig4/generate_ploscb_figs.m
@@ -0,0 +1,9 @@
+
+% cd(fullfile(mfiledir,'..'));
+
+%% generate elaz figure:
+% droso_ann(true,1,'all',false);
+droso_ann(true,2,3:4,false,[],1);
+
+%% generate orsi figure:
+droso_ann(true,2,1:2,false,[],2,4,-90)
